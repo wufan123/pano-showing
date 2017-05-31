8b3491b20fedc0a69cbb2aa5bc7743d65392ede7
吴华榕 <wuhuarong@star-net.cn>
2017-05-31 10:33:18 +0800

    补充请求字段

--- js/typeHouse.js
+++ js/typeHouse.js
@@ -1,4 +1,4 @@
-var openId, communityId;
+var openId, communityId,unionid,nickname,avatar;
 
 getData();
 
@@ -28,9 +28,12 @@ function haveToken() {
 function login() {
     communityId = "838945-1604-0636-0064";
     openId = "ouytHtzVh8PmfQQLkI3ittwEpYNA";
+    unionid="o4x5iszXtSPSOJ8WkbWP5ceIIuqo";
+    nickname="123456";
+    avatar="http%3A%2F%2Fwx.qlogo.cn%2Fmmopen%2FajNVdqHZLLAHV00sCqwMg8ibib8ZyPCF2EJLK7q88jSZ5yyj83KA28vdoWX2hBdaibpz9Y4nNFL625K1xbKwZ9M1w%2F0";
     // openId = getPar("openId");
     // communityId = getPar("communityId");
-    httpPost("/api/role/user/loginWx", {"wxOpenId": openId,"appId":1000}, function (res) {
+    httpPost("/api/role/user/loginWx", {"wxOpenId": openId,"appId":1000,"wxUnionId":unionid,"wxNick":nickname,"wxAvatarUrl":avatar}, function (res) {
         if(res.ret==0)
         {
             window.sessionStorage.setItem("token", res.token);
